8473b2784088c667d0ab7240b1ebaee02b28f7b3
kennisyoung <kennisyoung@gmail.com>
2019-10-14 23:19:02 +0800

    gif变动画

--- app.py
+++ app.py
@@ -270,7 +270,7 @@ def get_drop_id_name():
 
 if __name__ == '__main__':
     app.run(
-        host='127.0.0.1',
+        host='0.0.0.0',
         port=5000,
         threaded=True,
         debug=True
